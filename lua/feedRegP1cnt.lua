ac96a7eea5b70807a821f75f936bcc6ccb8dbd6c
Matt Greer <matt.e.greer@gmail.com>
2024-01-21 19:31:14 -0500

    lua script sets max positive/negative angles

--- lua/feedRegP1cnt.lua
+++ lua/feedRegP1cnt.lua
@@ -6,17 +6,21 @@ screen = manager.machine.screens[":screen"]
 
 REG_P1CNT = 0x300000
 
-RETURN_INPUT = false
+SETTING = 0
 
 function on_reg_read(offset, data) 
   if (offset == REG_P1CNT) then
-    if RETURN_INPUT then
-        -- 00010000
-        -- 11110000
-        return 0xf000 -- press a
-    else
-        return data
+    if (SETTING % 3) == 1 then
+        -- 01101111, negate since low is active -> 10010000
+        return 0x9000 -- lrud and bc, causing a value of 63, clamped to 60
     end
+
+    if (SETTING % 3) == 2 then
+        -- 11101111, negate since low is active -> 00010000
+        return 0x1000 -- lrud and bcd, causing a value of -63, clamped to -60
+    end
+
+    return data
   end
 end
 
@@ -30,14 +34,21 @@ function on_angle_write(offset, data)
 end
 
 reg_handler = mem:install_read_tap(REG_P1CNT, REG_P1CNT + 1, "reg", on_reg_read)
-angle_hanlder = mem:install_write_tap(ANGLE_ADDR, ANGLE_ADDR + 1, "angle", on_angle_write)
+angle_handler = mem:install_write_tap(ANGLE_ADDR, ANGLE_ADDR + 1, "angle", on_angle_write)
 
 function tick()
   keyboard_events.poll()
 end
 
 function on_frame()
-    local str = RETURN_INPUT and "returning input" or "not returning input"
+    str = "no forced input"
+    if SETTING == 1 then
+        str = "max positive"
+    end
+    if SETTING == 2 then
+        str = "max negative"
+    end
+
     screen:draw_text(0, 0, str, 0xffffffff, 0xff000000)
 
     if angle ~= nil then
@@ -50,7 +61,7 @@ emu.register_frame_done(on_frame, "frame")
 
 function on_t(e)
   if e == "pressed" then
-    RETURN_INPUT = not RETURN_INPUT
+    SETTING = (SETTING + 1) % 3
   end
 end
 
